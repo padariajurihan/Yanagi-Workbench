29ab234381c98c9d4702d53c8428aced3be78301
Tony Denalar <196391699+padariajurihan@users.noreply.github.com>
2026-07-14 17:40:47 -0300

    UPDATE: Improves the UI by adding a menu on the left side that displays the desired widget

--- main.py
+++ main.py
@@ -3,29 +3,34 @@ from tools.backup import BackupFrame
 from tools.console import ConsoleFrame
 from tools.image_converter import ImageConverterFrame
 from tools.config import APP_TITLE, APP_MIN_SIZE
+from tools.shared import ICONS
 
-class FileOperationsTab(ctk.CTkTabview):
+class ToolsColumn(ctk.CTkFrame):
     def __init__(self, master, **kwargs):
         super().__init__(master=master, **kwargs)
 
-        self.add('File Converter')
-        self.add('Backup')
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(0, weight=0)
+        self.grid_rowconfigure(1, weight=0)
+        self.grid_rowconfigure(2, weight=0)
+        self.grid_rowconfigure(3, weight=1)
 
-        # Configure the 'File Converter' tab to expand
-        image_converter_tab = self.tab('File Converter')
-        image_converter_tab.grid_rowconfigure(0, weight=1)
-        image_converter_tab.grid_columnconfigure(0, weight=1)
+        self.console_widget_btn = ctk.CTkButton(self, image=ICONS["console"], text="Console", compound="left", command=lambda: self._show_widget(ConsoleFrame))
+        self.console_widget_btn.grid(row=0, column=0, padx=10, pady=(10, 5), sticky="ew")
 
-        self.image_converter_frame = ImageConverterFrame(image_converter_tab)
-        self.image_converter_frame.grid(row=0, column=0, padx=10, pady=10, sticky='nsew')
+        self.backup_widget_btn = ctk.CTkButton(self, image=ICONS["backup"], text="Backup", compound="left", command=lambda: self._show_widget(BackupFrame))
+        self.backup_widget_btn.grid(row=1, column=0, padx=10, pady=5, sticky="ew")
 
-        # Configure the 'Backup' tab to expand
-        backup_tab = self.tab('Backup')
-        backup_tab.grid_rowconfigure(0, weight=1)
-        backup_tab.grid_columnconfigure(0, weight=1)
+        self.image_converter_widget_btn = ctk.CTkButton(self, image=ICONS["image_converter"], text="Image Converter", compound="left", command=lambda: self._show_widget(ImageConverterFrame))
+        self.image_converter_widget_btn.grid(row=2, column=0, padx=10, pady=(5, 10), sticky="ew")
 
-        self.backup_frame = BackupFrame(backup_tab)
-        self.backup_frame.grid(row=0, column=0, padx=10, pady=10, sticky='nsew')
+    def _show_widget(self, widget_class):
+        for widget in self.master.winfo_children():
+            if isinstance(widget, ctk.CTkFrame) and widget != self:
+                widget.destroy()
+
+        widget = widget_class(self.master)
+        widget.grid(row=0, column=1, padx=10, pady=10, sticky="nsew")
 
 
 class App(ctk.CTk):
@@ -34,13 +39,11 @@ class App(ctk.CTk):
 
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(0, weight=1)
-        self.grid_columnconfigure(1, weight=1)
-        
-        self.console_frame = ConsoleFrame(self)
-        self.console_frame.grid(row=0, column=0, padx=10, pady=10, sticky='nsew')
+        self.grid_columnconfigure(1, weight=3)
 
-        self.file_operations_tab = FileOperationsTab(self)
-        self.file_operations_tab.grid(row=0, column=1, padx=10, pady=10, sticky='nsew')
+        self.tools_column = ToolsColumn(self)
+        self.tools_column.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
+
 
 if __name__ == '__main__':
     root = App()
